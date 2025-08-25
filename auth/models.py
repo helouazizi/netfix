8635d65ecdce02c28471f8a86cfe0d2b75f84f52
helouazizi <ouazizi2code@gmail.com>
2025-08-25 16:07:51 +0100

    add a basic model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,13 @@
+# auth/models.py
 from django.db import models
 
 # Create your models here.
+
+
+
+class auth(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__ (self):
+        return self.name
--- netfix/settings.py
+++ netfix/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'users',
-    'services'
+    'services',
+    'auth'
 ]
 
 MIDDLEWARE = [
