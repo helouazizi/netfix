136115a3657420cc7faf9dc83ae6fa35b5a990b5
helouaziz <ouazizi2code@gmail.com>
2025-11-10 10:28:59 +0100

    remove prev version

--- services/forms.py
+++
@@ -1,8 +0,0 @@
-# services/forms.py
-from django import forms
-from .models import Service
-
-class ServiceForm(forms.ModelForm):
-    class Meta:
-        model = Service
-        exclude = ['created_at', 'company']
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/',views.login,name='login'),
-    path('register/',views.register,name='register'),
-    path('register/customer/', views.register_customer, name='register_customer'),
-    path('register/company/', views.register_company, name='register_company'),
-]
